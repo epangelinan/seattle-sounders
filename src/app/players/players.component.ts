2ae5874862f65afb2277059d39566c72f45592df
Esvethlynna Pangelinan <esveth@aol.com>
2017-08-25 09:52:17 -0700

    add player.model and add a hard-coded list of player info

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Player } from '../player.model';
 
 @Component({
   selector: 'app-players',
   templateUrl: './players.component.html',
   styleUrls: ['./players.component.css']
 })
-export class PlayersComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PlayersComponent {
+  players: Player[] = [
+    new Player("Osvaldo Alonso", "6 Midfielder", 31, "5' 11", 155, 1),
+    new Player("Clint Dempsey", "2 Forward ", 34, "6' 1", 170, 2),
+    new Player("Brad Evans", "3 Defender/Midfielder", 32, "6' 1", 170, 3)
+  ];
 }
