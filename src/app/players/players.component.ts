94727f006c55418bb06af8c8b2776831104f9662
Esvethlynna Pangelinan <esveth@aol.com>
2017-08-25 09:56:15 -0700

    update display of list of players

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -9,8 +9,8 @@ import { Player } from '../player.model';
 
 export class PlayersComponent {
   players: Player[] = [
-    new Player("Osvaldo Alonso", "6 Midfielder", 31, "5' 11", 155, 1),
-    new Player("Clint Dempsey", "2 Forward ", 34, "6' 1", 170, 2),
-    new Player("Brad Evans", "3 Defender/Midfielder", 32, "6' 1", 170, 3)
+    new Player("Osvaldo Alonso", "6 Midfielder", 31, "5'11", 155, 1),
+    new Player("Clint Dempsey", "2 Forward ", 34, "6'1", 170, 2),
+    new Player("Brad Evans", "3 Defender/Midfielder", 32, "6'1", 170, 3)
   ];
 }
